48dda19eacecd201cb7d46562ad198096ad6aade
user <gen1nya@gmail.com>
2025-06-12 23:14:04 +0300

    Updated dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,64 @@
 import React, { useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import styled, {createGlobalStyle} from 'styled-components';
 import { logout, openOverlay } from '../services/api';
 import { useNavigate } from 'react-router-dom';
+import Marquee from "react-fast-marquee";
 
+
+const GlobalStyle = createGlobalStyle`
+    html, body, #root {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        background: transparent !important;
+        overflow: hidden;
+    }
+    html, body {
+        scroll-behavior: auto;
+    }
+
+`;
 const Container = styled.div`
-    max-width: 600px;
-    margin: 80px auto;
     display: flex;
+    width: 100%;
+    box-sizing: border-box;
+    padding-left: 24px ;
+    padding-right: 24px;
     flex-direction: column;
     gap: 24px;
+`;
+
+const Wrapper = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+`;
+
+const Footer = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 24px;
+    background: #1e1e1e;
+    color: white;
+    font-size: 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px;
+    z-index: 999;
+    overflow: hidden;
+`;
+
+const Version = styled.span`
+    width: 80px;
+    text-align: start;
+    font-size: 12px;
+    color: #b0b0b0;
+    white-space: nowrap;
+    overflow: hidden;
 `;
 
 const Section = styled.section`
@@ -47,6 +97,18 @@ export default function Dashboard() {
         navigate('/settings', { replace: false });
     };
 
+    const handleCopyChatLink = () => {
+        const chatUrl = 'http://localhost:5173/chat-overlay';
+
+        navigator.clipboard.writeText(chatUrl)
+            .then(() => {
+                console.log('Ссылка скопирована!');
+            })
+            .catch((err) => {
+                console.error('Ошибка при копировании:', err);
+            });
+    };
+
     useEffect(() => {
         if (!called.current) {
             called.current = true;
@@ -55,38 +117,47 @@ export default function Dashboard() {
     }, []);
 
     return (
-        <Container>
-            <h2>👋 Вы авторизованы!</h2>
-            <Section>
-                <SectionTitle>Аккаунт</SectionTitle>
-                <p>Здесь будет отображаться информация об аккаунте.</p>
-                <ButtonsRow>
-                    <button onClick={handleLogout}>Выйти из аккаунта</button>
-                </ButtonsRow>
-            </Section>
+        <>
+            <GlobalStyle />
+            <Wrapper>
+                <Container>
+                    <h2>👋 Вы авторизованы!</h2>
+                    <Section>
+                        <SectionTitle>Аккаунт</SectionTitle>
+                        <p>Здесь будет отображаться информация об аккаунте.</p>
+                        <ButtonsRow>
+                            <button onClick={handleLogout}>Выйти из аккаунта</button>
+                        </ButtonsRow>
+                    </Section>
 
-            <Section>
-                <SectionTitle>Оверлей</SectionTitle>
-                <ButtonsRow>
-                    <button onClick={handleOpenOverlay}>Открыть чат в отдельном окне</button>
-                    <button>Скопировать ссылку на чат</button>
-                </ButtonsRow>
-            </Section>
+                    <Section>
+                        <SectionTitle>Оверлей</SectionTitle>
+                        <ButtonsRow>
+                            <button onClick={handleOpenOverlay}>Открыть чат в отдельном окне</button>
+                            <button onClick={handleCopyChatLink}>Скопировать ссылку на чат</button>
+                        </ButtonsRow>
+                    </Section>
 
-            <Section>
-                <SectionTitle>Быстрые действия со стримом</SectionTitle>
-                <ButtonsRow>
-                    <button disabled>Изменить категорию</button>
-                    <button disabled>Изменить теги</button>
-                    <button disabled>Забанить пользователя</button>
-                </ButtonsRow>
-            </Section>
+                    <Section>
+                        <SectionTitle>Быстрые действия со стримом</SectionTitle>
+                        <ButtonsRow>
+                            <button disabled>Изменить категорию</button>
+                            <button disabled>Изменить теги</button>
+                            <button disabled>Забанить пользователя</button>
+                        </ButtonsRow>
+                    </Section>
 
-            <Section>
-                <ButtonsRow>
-                    <button onClick={handlerOpenSettings}>Настройки</button>
-                </ButtonsRow>
-            </Section>
-        </Container>
+                    <Section>
+                        <ButtonsRow>
+                            <button onClick={handlerOpenSettings}>Настройки</button>
+                        </ButtonsRow>
+                    </Section>
+                </Container>
+                <Footer>
+                    <Marquee>Альфа-тест: ellis_leaf</Marquee>
+                    <Version>v0.0.2-alpha</Version>
+                </Footer>
+            </Wrapper>
+            </>
     );
 }
