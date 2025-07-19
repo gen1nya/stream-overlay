2949a54352fbe87e9a6ae019e968fad8d49ee0af
user <gen1nya@gmail.com>
2025-07-19 19:47:48 +0300

    🔖 version bump 1.2.1

--- src/components/app/Dashboard.jsx
+++ src/components/app/Dashboard.jsx
@@ -222,7 +222,7 @@ export default function Dashboard() {
                 </StatusBlock>
                 <Footer>
                     <Marquee>Бета-тест: ellis_leaf, kururun_chan, fox1k_ru</Marquee>
-                    <Version>v0.2.0-ts-alpha</Version>
+                    <Version>v0.2.1-ts-beta</Version>
                 </Footer>
             </Wrapper>
             </>
