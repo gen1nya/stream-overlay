8fd6cccca80ef37ef4d7e5aaca69a4ca74027c65
user <gen1nya@gmail.com>
2025-08-19 20:08:52 +0300

    version bump 0.4.2

--- src/components/app/Dashboard.jsx
+++ src/components/app/Dashboard.jsx
@@ -550,7 +550,7 @@ export default function Dashboard() {
                         </React.Fragment>
                     ))}
                 </Marquee>
-                <Version>v0.4.1-beta</Version>
+                <Version>v0.4.2-beta</Version>
             </Footer>
         </Wrapper>
     );
