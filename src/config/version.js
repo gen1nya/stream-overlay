774d95662f78b839dc628927b6a88c6f7e8ac1d2
user <gen1nya@gmail.com>
2025-12-19 13:08:00 +0200

    version bump 0.7.5-beta

--- src/config/version.js
+++ src/config/version.js
@@ -1,5 +1,5 @@
 // Auto-generated from version.json - do not edit manually
 // Run: npm run sync-version
 
-export const APP_VERSION = '0.7.2-beta';
-export const BUILD_DATE = '17.12.2025';
+export const APP_VERSION = '0.7.5-beta';
+export const BUILD_DATE = '19.12.2025';
